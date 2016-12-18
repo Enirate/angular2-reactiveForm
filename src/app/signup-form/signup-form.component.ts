810cb86bb98d603ae814b094da8560f2b0e2d140
Enirate <enirate@gmail.com>
2016-12-18 01:11:44 +0100

    bindings done, valudation rules done

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -11,14 +11,19 @@ export class SignupFormComponent implements OnInit {
   user: FormGroup; //defining our user type as an object.
   constructor() { }
 
-  ngOnInit() { //this arrangement/field must tally with the one in the DOM 
+  ngOnInit() { //this arrangement/field must tally with the one in the DOM we bind this object with the DOM fields using
+    //[FormGroup] = "user" i.e. object name.
     this.user = new FormGroup({
-      name: new FormControl(''),
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),//form control object accepts two values, 
+      //1. field placeholder. 2. rules(using the Validator class. Notices the use of array to hold multiple rules).
       account: new FormGroup ({
-        email: new FormControl(''),
-        confirm: new FormControl('')
+        email: new FormControl('', Validators.required),
+        confirm: new FormControl('', Validators.required)
       })
     });
+  }
+  onSubmit({value, valid}: {value:User, valid:boolean}) { //method that will be activated when the submit button is clicked
+    console.log(value, valid);
   }
 
 }
